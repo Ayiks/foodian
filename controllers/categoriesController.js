8a3e0b3fea95d9cc068623fd2e36fb363abe445b
Ayiks <cliffysarp@gmail.com>
2021-11-13 11:37:02 +0000

    update of json body format

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -4,7 +4,11 @@ import { categoryModel } from "../models/category.js";
 const getAllCategories = async (req, res) => {
     try {
         const categoriesList = await categoryModel.find({});
-        return res.json(categoriesList)
+        return res.status(200).json({
+            status: true,
+            message: 'fetched succesfully',
+            data: categoriesList
+        })
     } catch (error) {
         console.log('Somthing went wrong: ', error);
         res.status(400).send('Failed to fetch data', error)
